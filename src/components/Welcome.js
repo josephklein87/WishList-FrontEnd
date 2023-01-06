76a99ae69fda3ae7696e81e1f4a23ece6ea2bafb
General Assembly <generalassembly@tests-MacBook-Air.local>
2023-01-05 19:54:29 -0500

    initial welcome screen pretty much done. need some assistance with the mobile view because of the icon size

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,12 +5,12 @@ const Welcome = () => {
 
     return (
         <div className="welcome-box">
-            <div className="icon">
+            <div className="welcome-icon">
                 <AiFillGift className='gift-icon' size={'20em'} />
             </div>
             <div className="welcome-container">
                 <h1 className="bigTitle">WSHLST</h1>
-                <h3 className="smallTitle">The wishlist app</h3>
+                <h3 className="smallTitle">Gifts Minus The Guesswork</h3>
                 <h4 className="smallestTitle">Login to continue</h4>
             </div>
         </div>
